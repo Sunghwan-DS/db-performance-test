51739c548d42f7d0090dbd76da37f537e942407a
Sunghwan-DS <jeonsung02@gmail.com>
2023-07-10 11:11:59 +0900

    Create Spring Project

---
+++ src/main/java/jsh/dbperformancetest/DBPerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package jsh.dbperformancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBPerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBPerformanceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/dbperformancetest/DbPerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.dbperformancetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPerformanceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
